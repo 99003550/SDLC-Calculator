c64304e406108842a7a9cb3a228124d4fa282d34
stepin105057 <69064298+stepin105057@users.noreply.github.com>
2021-02-05 16:39:26 +0530

    bitwise and,or src inc

---
+++ Implementation/src/calculator.c
@@ -0,0 +1,13 @@
+#include "calculator.h"
+#include<math.h>
+#define pi 3.14
+
+int bitwise_or(int num1, int num2)
+{
+    return num1|num2;
+}
+
+int bitwise_and(int num1, int num2)
+{
+    return num1&num2;
+}
