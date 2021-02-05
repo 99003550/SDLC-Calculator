df575d0ffcee61f63f668452ecb881db77f2b980
stepin104955 <69070462+stepin104955@users.noreply.github.com>
2021-02-05 17:13:15 +0530

    Update calculator.c

--- Implementation/src/calculator.c
+++ Implementation/src/calculator.c
@@ -2,12 +2,12 @@
 #include<math.h>
 #define pi 3.14
 
-int add(int num1,int num2)
+float add(float num1,float num2)
 {
     return num1+num2;
 }
 
-int sub(int num1,int num2)
+float sub(float num1,float num2)
 {
     return num1-num2;
 }
