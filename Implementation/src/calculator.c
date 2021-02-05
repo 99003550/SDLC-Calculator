08bc0362140de461bc19663055d29bb0ab7ee86d
midhun <midhunmmenon.mec@gmail.com>
2021-02-05 17:53:18 +0530

    Update calculator.c

--- Implementation/src/calculator.c
+++ Implementation/src/calculator.c
@@ -1,31 +1,51 @@
-#include<calculator.h>
+#include "calculator.h"
 #include<math.h>
 #define pi 3.14
 
 
-int add(int num1,int num2)
+float add(float num1,float num2)
+
 {
     return num1+num2;
 }
 
-int sub(int num1,int num2)
+
+float sub(float num1,float num2)
 {
     return num1-num2;
 }
 
-int bitwise_or(int num1, int num2)
+
+float square(float num1)
 {
-    return num1*num2;
+    return (num1*num1);
+}
+
+float cube(float num1)
+{
+    float result;
+    result=pow(num1,3);
+    return result;
 }
 
 
 float multiply(float num1, float num2)
+
 {
-    return num1*num2;
+    return num1&num2;
 }
+
+
 float divide(float num1, float num2)
 {
+<<<<<<< HEAD
     if(num2 == 0)
+=======
+
+
+    if(num2==0)
+
+>>>>>>> b60dd4226b198745c5654a7bddaefd656aa48593
     {
         return -1;
     }
@@ -33,40 +53,43 @@ float divide(float num1, float num2)
     {
         return num1/num2;
     }
+<<<<<<< HEAD
     return num1*num2;
+=======
+
+    return num1&num2;
+>>>>>>> b60dd4226b198745c5654a7bddaefd656aa48593
+}
+int bitwise_or(int num1, int num2)
+
+
+{
+    return num1|num2;
 }
 
+int bitwise_and(int num1, int num2)
 
-int Xor(int num1, int num2)
+
+{
+    return num1&num2;
+}
+float Xor(int num1, int num2)
 {
     return num1^num2;
 }
-
-int logarithemicvalue(float num1)
+float logarithemicvalue(float num1)
 {
     return log(num1);
 }
-
 float log_base10(float num1)
 {
     return log10(num1);
 }
-
-float fabsolute(float num1)
-{
-    return fabs(num1);
-}
-
-float sin(float num1)
-{ 
-    return sin(num1);
-}
-
-float cos(float num1)
-{
-    return cos(num1);
-
-}
+ 
+ float fabsolute(float num1)
+ {
+     return fabs(num1);
+ }
 
 float factorial(float num1)
 {
@@ -79,22 +102,18 @@ float factorial(float num1)
      return num1*factorial(num1-1);
     }
 }
-
-float power(float num1, float num2)
+double exponent(double num1)
 {
-    if(num2==0)
-    {
-        return 1;
-    }
-    else
-    {
-       return pow(num1, num2);
-    }
+
+    return exp(num1);
 }
-
-
-
-
+float sin(float num1)
+{
+    return sin(num1);
+}
+float cos(float num1)
+{
+    return cos(num1);
 }
 float tan(float num1)
 {
@@ -106,6 +125,15 @@ float tan(float num1)
     {
         return tan(num1);
     }
-
 }
-
+float power(float num1, float num2)
+{
+    if(num2==0)
+    {
+        return 1;
+    }
+    else
+        {
+       return pow(num1, num2);
+    }
+}
