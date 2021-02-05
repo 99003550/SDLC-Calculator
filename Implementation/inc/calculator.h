1e11cbbbe0083be498f2b257dc5407426b0f9d9b
midhun <midhunmmenon.mec@gmail.com>
2021-02-05 15:43:48 +0530

    Update calculator.h

---
+++ Implementation/inc/calculator.h
@@ -0,0 +1,13 @@
+#ifndef CALCULATOR_H_INCLUDED
+#define CALCULATOR_H_INCLUDED
+#ifndef __CALCULATOR_H__
+#define __CALCULATOR_H__
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+float multiply(float num1,float num2);
+float divide(float num1,float num2);
+
+#endif
