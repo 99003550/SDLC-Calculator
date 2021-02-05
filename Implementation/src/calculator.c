00c9a6b3eb49cddc9b6082b8af1654ead495c9f9
anushakurasala <anushakurasala@gmail.com>
2021-02-05 17:37:06 +0530

    Update calculator.c

--- Implementation/src/calculator.c
+++ Implementation/src/calculator.c
@@ -81,3 +81,22 @@ int bitwise_and(int num1, int num2)
 {
     return num1&num2;
 }
+float sin(float num1)
+{
+    return sin(num1);
+}
+float cos(float num1)
+{
+    return cos(num1);
+}
+float tan(float num1)
+{
+    if(num1==pi/2)
+    {
+        return -1;
+    }
+    else
+    {
+        return tan(num1);
+    }
+}
