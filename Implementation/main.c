8fd9d74303d3eda189203c98fc8ca06ec6742363
99003541 <78578684+99003541@users.noreply.github.com>
2021-02-06 16:09:00 +0530

    Update main.c

--- Implementation/main.c
+++ Implementation/main.c
@@ -3,7 +3,7 @@
 #include <calculator.h>
 float (*p[10])(float x);
 float (*q[9])(float x,float y);
-int (*bit_funcs[3])(float x,float y);
+int (*bit_funcs[3])(int x,int y);
 
 int main(void)
 {
@@ -47,7 +47,7 @@ int main(void)
   {
       printf("Enter numbers: ");
       scanf("%f %f", &num1, &num2);
-      result = (*q[op -10](num1 , num2);
+      result = (*q[op-10](num1 , num2);
       printf("result: %f", result);
     }
       if(op>15 && op<19)
