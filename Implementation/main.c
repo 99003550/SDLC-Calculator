91a0ee527c330bb0cc1d0eb9aa85435450436476
99003540 <78578595+99003540@users.noreply.github.com>
2021-02-06 15:20:53 +0530

    Update main.c

--- Implementation/main.c
+++ Implementation/main.c
@@ -3,6 +3,7 @@
 #include <calculator.h>
 float (*p[10])(float x);
 float (*q[18])(float x,float y);
+float (*bit_funcs[20])(float x,float y);
 int main(void)
 {
   float result;
@@ -14,19 +15,19 @@ int main(void)
   q[2] = multiply;
   q[3] = divide;
   q[4] = power;
-  q[5] = bitwise_or;
-  q[6] = bitwise_and;
+  bit_funcs[5] = bitwise_or;
+  bit_funcs[6] = bitwise_and;
   q[7] = Xor;
   p[8] = square;
   p[9] = cube;
-  p[0] = sin1;
-  p[1] = cos1;
-  p[2] = tan1;
-  p[3] = factorial;
-  p[4] = logarithemicvalue;
-  p[5] = exponential;
-  p[6] = log_base10;
-  p[7] = fabsolute;
+  p[10] = sin1;
+  p[11] = cos1;
+  p[12] = tan1;
+  p[13] = factorial;
+  p[14] = logarithemicvalue;
+  p[15] = exponential;
+  p[16] = log_base10;
+  p[17] = fabsolute;
   
   printf("0:add;1:sub;2:multiply;3:divide;4:power;5:bitwise_or;6:bitwise_and;7:Xor;8:square;9:cube;10:sin/n;11:cos;12:tan;13:factorial;14:logarithemicvalue;15:exponential;16:log_base10;17:fabsolute\n");
   do {
@@ -44,6 +45,11 @@ int main(void)
   {
       printf("Enter numbers: ");
       scanf("%f %f", &num1, &num2);
+      if(op=5 ||op=6)
+      {
+        result=(*bit_funcs[op])(num1,num2);
+        break;
+      }
       result = (*q[op])(num1, num2);
       printf("result : %f", result);
 
