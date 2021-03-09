26a1f8a9b0390bcf48c41d4ca2ac2bd26df2d57b
99003549 <78538040+99003549@users.noreply.github.com>
2021-03-09 15:21:28 +0530

    Update calculator.h

--- Implementation/inc/calculator.h
+++ Implementation/inc/calculator.h
@@ -1,37 +1,4 @@
-/*#ifndef CALCULATOR_H_INCLUDED
-#define CALCULATOR_H_INCLUDED
-#ifndef __CALCULATOR_H__
-#define __CALCULATOR_H__
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
-
-float add(float num1,float num2);
-float sub(float num1,float num2);
-float multiply(float num1,float num2);
-float divide(float num1,float num2);
-float power(float num1, float num2);
-int bitwise_or(int num1, int num2);
-int bitwise_and(int num1, int num2);
-int Xor(int num1, int num2);
-float square(float num1);
-float cube(float num1);
-float sin1(float num1);
-float cos1(float num1);
-float tan1(float num1);
-float factorial(float num1);
-float logarithemicvalue(float num1);
-float exponential(float num1);
-float log_base10(float num1);
-float fabsolute(float num1);
-
-
-
-
-
-#endif
-#endif*/
 
 #ifndef CALCULATOR_H_INCLUDED
 #define CALCULATOR_H_INCLUDED
