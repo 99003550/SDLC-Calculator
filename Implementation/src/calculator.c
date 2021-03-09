9c0446b2ef0f3084af207a36da326192023c2073
99003549 <78538040+99003549@users.noreply.github.com>
2021-03-09 14:45:44 +0530

    Update calculator.c

--- Implementation/src/calculator.c
+++ Implementation/src/calculator.c
@@ -1,126 +1,4 @@
-/*#include "calculator.h"
-#include<math.h>
-#define pi 3.14
 
-
-float add(float num1,float num2)
-{
-    return num1+num2;
-}
-
-
-float sub(float num1,float num2)
-{
-    return num1-num2;
-}
-
-
-float multiply(float num1, float num2)
-
-{
-    return num1*num2;
-}
-
-
-float divide(float num1, float num2)
-{
-
-    if(num2 == 0)
-
-
-    {
-        return -1;
-    }
-    else
-    {
-        return num1/num2;
-    }
-float power(float num1, float num2)
-{
-    if(num2==0)
-    {
-        return 1;
-    }
-    else
-        {
-       return pow(num1, num2);
-    }
-}
-int bitwise_or(int bit_num1, int bit_num2)
-
-
-{
-    return bit_num1|bit_num2;
-}
-
-int bitwise_and(int bit_num1, int bit_num2)
-
-
-{
-    return bit_num1&bit_num2;
-}
-int Xor(int bit_num1, int bit_num2)
-{
-    return bit_num1^bit_num2;
-}
-    
-
-}
-float square(float num1)
-{
-    return (num1*num1);
-}
-
-float cube(float num1)
-{
-    float result;
-    result=pow(num1,3);
-    return result;
-}
-
-float sin1(float num1)
-{
-    return sin(num1);
-}
-float cos1(float num1)
-{
-    return cos(num1);
-}
-float tan1(float num1)
-{
-    if(num1==pi/2)
-    {
-        return -1;
-    }
-    else
-    {
-        return tan(num1);
-    }
-}
-//float factorial(float num1){
-
-//}
-
-float logarithemicvalue(float num1)
-{
-    return log(num1);
-}
-float exponent(float num1)
-{
-
-    return exp(num1);
-}
-
-
-float log_base10(float num1)
-{
-    return log10(num1);
-}
- 
- float fabsolute(float num1)
- {
-     return fabs(num1);
- }*/
 #include "calculator.h"
 #include<math.h>
 #define pi 3.14
@@ -163,14 +41,9 @@ float divide(float num1, float num2)
 }
 float factorial(float num1)
 {
-    if (num1 == 0)
-        {
-        return 1;
-       }
-    else
-    {
+    
      return num1*factorial(num1-1);
-    }
+    
 }
 float power(float num1, float num2)
 {
